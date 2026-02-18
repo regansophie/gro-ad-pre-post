1c11f9b7e8648fcc03f7eba76736454208fc2713
Sophie Regan <126125156+regansophie@users.noreply.github.com>
2026-02-18 14:25:36 -0800

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1377,12 +1377,12 @@ console.log(speaker_con);
 timeline.push(preload_images);
 
 // Uncomment for RPP
-// timeline.push(opening_instructions);
+ timeline.push(opening_instructions);
 
 
 // Uncomment for Prolific
- timeline.push(prolific_id_page);
- timeline.push(opening_instructions_prolific);
+// timeline.push(prolific_id_page);
+// timeline.push(opening_instructions_prolific);
 
 
 timeline.push(consent_block);
@@ -1428,10 +1428,10 @@ timeline.push(saving_screen);
 timeline.push(save_data);
 
 // Uncomment for Prolific
-timeline.push(prolific_completion_page);
+//timeline.push(prolific_completion_page);
 
 // Uncomment for RPP
-// timeline.push(credit_instructions);
+timeline.push(credit_instructions);
 
 
 // ==================================================
